11d661eab93f3d23ee5f560855f19d490997fa1c
JhosepString <2411080083@undc.edu.pe>
2025-11-18 10:03:26 -0500

    Commit creado para Mongo

---
+++ constante.js
@@ -0,0 +1,15 @@
+const DB_USER= "Jhosep";
+const DB_PASSWORD= "8904180";
+const DB_HOST= "web-mongo.d5r5bf.mongodb.net";
+
+const API_VERSION = "v1";
+const IP_SERVER= "lolcahost";
+
+module.exports = {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+    JWT_SECRET_KEY,
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const {
+
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    IP_SERVER,
+    API_VERSION,
+
+
+} = require ('./constante');
+
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`,
+    (error) => {
+        if (error) throw error;
+            console.log('conexión exitosa');
+        
+        }
+);
